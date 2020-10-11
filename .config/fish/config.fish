9eb523cee5f57115dde05fea5d9e7e6b0891efbd
Stephen Li <me@ziyunli.dev>
2020-10-11 18:30:27 -0400

    add cargo to fish profile

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,22 @@
 set PYENV_ROOT ~/.pyenv
 set GOPATH ~/go
+set CARGO_PATH ~/.cargo/bin
 set RBENV_ROOT  ~/.rbenv/shims/
 set FLUTTER_PATH ~/flutter/bin
-set PATH $PATH ~/bin $FLUTTER_PATH $GOPATH $RBENV_ROOT $PYENV_ROOT /usr/local/opt/curl/bin /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin
+set PATH $PATH \
+    ~/bin \
+    $FLUTTER_PATH \
+    $CARGO_PATH \
+    $GOPATH \
+    $RBENV_ROOT \
+    $PYENV_ROOT \
+    /usr/local/opt/curl/bin \
+    /usr/local/bin \
+    /usr/bin \
+    /bin \
+    /usr/local/sbin \
+    /usr/sbin \
+    /sbin
 
 # Completion for Kitty
 kitty + complete setup fish | source
