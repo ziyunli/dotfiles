8c15ffb7ce21d95f51ea4c0e4eec299de6d40c06
Ziyun Li <me@ziyunli.dev>
2023-05-03 00:49:15 -0700

    update steup linux script

--- setup_linux.sh
+++ setup_linux.sh
@@ -9,42 +9,23 @@ postgresql postgresql-contrib postgresql-client
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-[[ ! -d $ZSH_CUSTOM/themes ]] && mkdir $ZSH_CUSTOM/themes\
-git clone https://github.com/agkozak/agkozak-zsh-prompt $ZSH_CUSTOM/themes/agkozak\
+[[ ! -d $ZSH_CUSTOM/themes ]] && mkdir $ZSH_CUSTOM/themes
+git clone https://github.com/agkozak/agkozak-zsh-prompt $ZSH_CUSTOM/themes/agkozak
 ln -s $ZSH_CUSTOM/themes/agkozak/agkozak-zsh-prompt.plugin.zsh $ZSH_CUSTOM/themes/agkozak.zsh-theme
 
 # zsh-syntax-highlighting
+[[ ! -d $ZSH_CUSTOM/themes ]] && mkdir $ZSH_CUSTOM/plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# python
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 
 # fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ./.fzf/install
 
-# nvm
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-nvm install --lts
-
 # rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install exa ripgrep git-delta zoxide
 
-# asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
-
-asdf plugin add python
-asdf install python latest
-
-asdf plugin add erlang
-asdf install erlang latest
-
-asdf plugin-add elixir
-asdf install elixir latest
-
 # Python Poetry
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-mkdir $ZSH_CUSTOM/plugins/poetry
-poetry completions zsh > $ZSH_CUSTOM/plugins/poetry/_poetry
+curl -sSL https://install.python-poetry.org | python3 -
+[[ ! -d ~/.zfunc ]] && mkdir ~/.zfunc
+poetry completions zsh > ~/.zfunc/_poetry
